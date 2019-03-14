69d07dd23e6ea8de178ff5491e328223b6969195
dcl15 <dcl15@iMac-de-iMac15.local>
2019-03-14 10:23:12 -0600

    Problema 00

--- Medias.cs
+++ Medias.cs
@@ -4,24 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace AutomatizarPruebasUnitarias {
-    
+
     class Medias {
 
         /**
          * Calcula y regresa la media artimética
          */
-        public static double mediaAritmetica(params int[] vals) { }
+        public static double mediaAritmetica(int[] vals) {
+          int suma = 0;
+          int resultado = 0;
+          for(int i = 0; i <= vals.Length; i++) {
+            suma += vals[i];
+          }
+          return resultado = suma/vals.Length;
+        }
 
         /**
          * Calcula y regresa la raíz enésima = x^(1/n)
          */
-        private static double raizEnesima(double x, int n) { }
+        private static double raizEnesima(double x, int n) {
+          return x^(1/n);
+        }
 
         /**
          *  Usa raizEnesima para calcular y regresar la media geométrica
          */
-        public double mediaGeometrica(params int[] vals) { }
+        public double mediaGeometrica(int[] vals) {
+          int suma = 0;
+          for(int i=0; i < vals.Length; i++) {
+            suma += 1/vals[i];
+          }
+          return vals.Length/suma;
+        }
 
         /**
          * Este método no está implementado
