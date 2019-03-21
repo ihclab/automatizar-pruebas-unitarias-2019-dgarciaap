e9f24c3cfa68a1980689da7bc6f039448aec4ac6
dgarciaap <dgarcia24@ucol.mx>
2019-03-21 15:18:04 -0600

    Problema 02

--- Main.js
+++ Main.js
@@ -1,1 +1,10 @@
+const Fs = require('fs');
 
+Fs.readFile('./CasosPrueba.txt', 'utf-8', (err, data) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log(data);
+    }
+})
